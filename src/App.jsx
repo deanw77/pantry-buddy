e342f2bbac405eeb7779faec799ae98c9ebbb5f4
deanw77 <deanwaring77@outlook.com>
2024-02-03 13:53:07 +0000

    Initial Commit, Base React Files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <iframe 
+        src="https://giphy.com/embed/m7y2uwgclFzc4" width="480" height="359" className="giphy-embed" allowFullScreen>
+      </iframe>
+    </>
+  )
+}
+
+export default App
