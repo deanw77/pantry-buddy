14d222927b8b98a760c565dce736a41137c0f325
Alexandra Asanache <ale.asanache@gmail.com>
2024-02-07 20:44:19 +0000

    Finished SideNav component, needs linking to pages

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -1,7 +1,28 @@
+import Dashboard from "./auth/Dashboard";
+import SideNav, { SidebarItem } from "./nav/SideNav";
+
+//import the needed icons from lucide react
+import {
+  CookingPot,
+  ClipboardList,
+  LayoutGrid,
+  Sandwich,
+  CircleUserRound,
+  Leaf,
+} from "lucide-react"
+
 export default function UserData() {
   return (
     <>
-      <h1>User Page will appear here</h1>
-    </>
+      <SideNav>
+        <SidebarItem
+        icon={<LayoutGrid size={20} />}  text="Dashboard" active />
+        <SidebarItem icon = {<Sandwich size={20}/>}  text="My Pantry"/>
+        <SidebarItem icon={<CookingPot size={20}/>}  text="Recipes" alert/>
+        <SidebarItem icon={<ClipboardList size={20}/>}  text="Grocery List"/>
+        <SidebarItem icon={<Leaf size={20}/>}  text="Carbon Footprint"/>
+        <SidebarItem icon={<CircleUserRound size={20}/>}  text="Account"/>
+      </SideNav>
+      </>
   );
 }
