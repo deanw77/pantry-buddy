32a68c543684b387af4e89eaeb7e765575c365e0
Alexandra Asanache <ale.asanache@gmail.com>
2024-02-08 22:21:19 +0000

    Added MyPantry page to work in, needs work + Get Recipe CTA Component

--- src/components/MyPantry.jsx
+++ src/components/MyPantry.jsx
@@ -1,37 +1,39 @@
+// Imports Displayed on All Pages
+import SideNav, { SidebarItem } from "./nav/SideNav";
+import Footer from "./nav/Footer";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
 import TestWidget from "./widgets/TestWidget";
 import Profile from "./widgets/Profile.jsx";
 
-import SideNav, { SidebarItem } from "./nav/SideNav";
-
 //import the needed icons from lucide react
 import {
-  CookingPot,
-  ClipboardList,
-  LayoutGrid,
-  Sandwich,
-  CircleUserRound,
-  Leaf,
-} from "lucide-react";
-import Footer from "./nav/Footer";
+    CookingPot,
+    ClipboardList,
+    LayoutGrid,
+    Sandwich,
+    CircleUserRound,
+    Leaf,
+  } from "lucide-react";
 
-export default function UserData() {
-  const [active, setActive] = useState("TestWidget");
+//Widgets for this page specifically
+import GetRecipeCTA from "./widgets/GetRecipeCTA";
 
-  return (
-    <>
-      <div className="flex w-full pageContent">
-        <SideNav>
+export default function MyPantry() {
+    const [active, setActive] = useState("TestWidget");
+
+    return (
+        <>
+       <div className="flex">
+       <SideNav>
           <button className="text-left" onClick={() => setActive("TestWidget")}>
             <SidebarItem
               icon={<LayoutGrid size={20} />}
               text="Dashboard"
-              active
             />
           </button>
-          <Link to="/mypantry"><SidebarItem icon={<Sandwich size={20} />} text="My Pantry" /></Link>
+          <Link to="/mypantry"><SidebarItem icon={<Sandwich size={20} />} text="My Pantry" active/></Link>
           <SidebarItem icon={<CookingPot size={20} />} text="Recipes" alert />
           <SidebarItem icon={<ClipboardList size={20} />} text="Grocery List" />
           <SidebarItem icon={<Leaf size={20} />} text="Carbon Footprint" />
@@ -42,13 +44,15 @@ export default function UserData() {
           />
           </button>
         </SideNav>
-
-        <div className="w-full pageContent">
-          {active === "TestWidget" && <TestWidget title="TestWidget" />}
-          {active === "Profile" && <Profile title="Profile" />}
-          <Footer />
+        <div className="flex flex-col">
+        <GetRecipeCTA/>
+        
         </div>
-      </div>
-    </>
-  );
+       </div>
+       <footer>
+        <Footer/>
+        </footer>
+        </>
+    )
 }
+
