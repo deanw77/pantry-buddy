5b9d9fc7ef752631d73a3c3005f213695017c24e
Ajaya Bhogal-Todd <ajaya@Ajayas-MacBook-Pro.local>
2024-02-09 19:16:39 +0000

    Added API logic for spoonacular and edited card recipe returned values

--- src/components/widgets/CarbonFootprint.jsx
+++ src/components/widgets/CarbonFootprint.jsx
@@ -1,9 +1,20 @@
+import SearchBarRecipeParent from "./SearchBarRecipeParent";
+import PantryEntryForm from "./PantryEntryForm";
+
+
 function CarbonFootprint() {
     return ( 
         <div id="widgetContainer" className="flex">
             <h1>Carbon Footprint Display</h1>
+            <SearchBarRecipeParent />
+            <PantryEntryForm />
         </div>
+          
+        
      );
+   
 }
+
+
 
 export default CarbonFootprint;
