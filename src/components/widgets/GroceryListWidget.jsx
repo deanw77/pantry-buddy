04ecbc69dc276656c60a5c69d9e924e871e5c7af
Ajaya Bhogal-Todd <ajaya@Ajayas-MacBook-Pro.local>
2024-02-12 12:48:41 +0000

    Added foodprintAPI with average carbon output for queried item. Added as button to grocerylist widget

--- src/components/widgets/GroceryListWidget.jsx
+++ src/components/widgets/GroceryListWidget.jsx
@@ -1,75 +1,164 @@
-// Import Functions from React
-import { useState, useEffect } from "react";
+// // Import Functions from React
+// import { useState, useEffect } from "react";
 
-// Import Required Functions fom FireBase
+// // Import Required Functions fom FireBase
+// import { auth, db } from "../../firebase/firebase";
+// import { collection, query, where, getDocs, doc, setDoc, updateDoc, deleteField, } from "firebase/firestore";
+
+// function GroceryList() {
+//   // States required to Store Form Entry and Database information
+//   const [foodEntry, setFoodEntry] = useState("");
+//   const [userGroceryList, setUserGroceryList] = useState([]);
+
+//   // Store Current Users UID
+//   const user = auth.currentUser.uid;
+
+//   // Fetch the users Shopping List from Database
+//   const fetchPantryListOnce = async () => {
+//     const q = query(collection(db, "groceryList"),  where("name", "==", `${user}`));
+//     const querySnapshot = await getDocs(q);
+
+//     // Store the Shopping List from the database into the userGroceryList variable.
+//     querySnapshot.forEach((doc) => {
+//       setUserGroceryList(doc.data());
+//       // This removes the uid field from the userGroceryList Array
+//       setUserGroceryList((current) => {
+//         // eslint-disable-next-line no-unused-vars
+//         const { name, ...rest } = current;
+//         return rest;
+//       });
+//     });
+//   };
+
+//     // Run the Database inside of useEffect so they don't run repeatedly
+//   useEffect(() => {
+//     fetchPantryListOnce();
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, []);
+
+//   // Take the data from the input field and add it to the users database.
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     const valRef = doc(db, "groceryList", `${auth.currentUser.uid}`);
+//     await setDoc(valRef, { [foodEntry]: `${foodEntry}` }, { merge: true });
+//     // Once we have added the data, we will to call the fetch data function to update the Shopping List variable
+//     fetchPantryListOnce();
+
+//     // Clear the form field
+//     setFoodEntry("");
+//   };
+
+//   // Delete Item from Shooping List DataBase onClick event
+//   const deleteGroceryItem = async (value) => {
+//     // Create an array contents of userGroceryList Variable
+//     const current = { ...userGroceryList };
+//     // Delete the value passed, this will corespond to the item being deleted
+//     delete current[value];
+//     // Set the Pantry List to the new set of a values
+//     setUserGroceryList(current);
+
+//     // Delete the same value passed from the Database
+//     const valRef = doc(db, "groceryList", `${auth.currentUser.uid}`);
+//     const data = { [value]: deleteField() };
+//     updateDoc(valRef, data);
+//   };
+
+//   return (
+//     <div className="bg-white p-6 flex flex-col justify-between">
+
+//       <h1 className="text-3xl font-bold text-green-600">
+//         My Grocery List
+//       </h1>
+
+//       <ul id="PantryList">
+//         {Object.keys(userGroceryList).map((key, index) => (
+//           <li key={index} className="flex justify-between align-center m-1">
+//             <span id="PantryListItem" className="p-1 pl-5">{key}</span>
+//             <a onClick={() => deleteGroceryItem(key)}>
+//               <div className="bg-red-700 flex align-center justify-center rounded-full">  
+//                 <span id="PantryListDelete" className="pr-3 m-0 text-white">
+//                   X
+//                 </span>
+//               </div>
+//             </a>
+//           </li>
+//         ))}
+//       </ul>
+
+//       <label htmlFor="FoodName" className="font-semibold">
+//         Food Name: 
+
+//         <input
+//           type="text"
+//           value={foodEntry}
+//           onChange={(e) => setFoodEntry(e.target.value)}
+//           className="inline-block bg-gray-100 text-gray-700 border border-amber-600 rounded py-3 px-4 ml-5 leading-tight focus:bg-white focus:border-green-600 mb-3"
+//         />
+//       </label>
+
+//       <button
+//         onClick={handleSubmit}
+//         className="w-full justify-center rounded-md bg-green-600 p-2 my-3 text-sm font-semibold leading-6 text-black shadow-lg hover:bg-amber-500"
+//       >
+//         Add Item To Shopping List
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default GroceryList;
+
+import React, { useState, useEffect } from "react";
 import { auth, db } from "../../firebase/firebase";
-import { collection, query, where, getDocs, doc, setDoc, updateDoc, deleteField, } from "firebase/firestore";
-
+import { collection, query, where, getDocs, doc, setDoc, updateDoc, deleteField } from "firebase/firestore";
+import FoodPrintApi from "./FoodprintApi";
 function GroceryList() {
-  // States required to Store Form Entry and Database information
   const [foodEntry, setFoodEntry] = useState("");
   const [userGroceryList, setUserGroceryList] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(""); // State for tracking the selected item for FoodPrintAPI
 
-  // Store Current Users UID
   const user = auth.currentUser.uid;
 
-  // Fetch the users Shopping List from Database
   const fetchPantryListOnce = async () => {
-    const q = query(collection(db, "groceryList"),  where("name", "==", `${user}`));
+    const q = query(collection(db, "groceryList"), where("name", "==", `${user}`));
     const querySnapshot = await getDocs(q);
 
-    // Store the Shopping List from the database into the userGroceryList variable.
     querySnapshot.forEach((doc) => {
       setUserGroceryList(doc.data());
-      // This removes the uid field from the userGroceryList Array
       setUserGroceryList((current) => {
-        // eslint-disable-next-line no-unused-vars
         const { name, ...rest } = current;
         return rest;
       });
     });
   };
 
-    // Run the Database inside of useEffect so they don't run repeatedly
   useEffect(() => {
     fetchPantryListOnce();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // Take the data from the input field and add it to the users database.
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const valRef = doc(db, "groceryList", `${auth.currentUser.uid}`);
     await setDoc(valRef, { [foodEntry]: `${foodEntry}` }, { merge: true });
-    // Once we have added the data, we will to call the fetch data function to update the Shopping List variable
     fetchPantryListOnce();
 
-    // Clear the form field
     setFoodEntry("");
   };
 
-  // Delete Item from Shooping List DataBase onClick event
   const deleteGroceryItem = async (value) => {
-    // Create an array contents of userGroceryList Variable
     const current = { ...userGroceryList };
-    // Delete the value passed, this will corespond to the item being deleted
     delete current[value];
-    // Set the Pantry List to the new set of a values
     setUserGroceryList(current);
 
-    // Delete the same value passed from the Database
     const valRef = doc(db, "groceryList", `${auth.currentUser.uid}`);
-    const data = { [value]: deleteField() };
-    updateDoc(valRef, data);
+    await updateDoc(valRef, { [value]: deleteField() });
   };
 
   return (
     <div className="bg-white p-6 flex flex-col justify-between">
-
-      <h1 className="text-3xl font-bold text-green-600">
-        My Grocery List
-      </h1>
+      <h1 className="text-3xl font-bold text-green-600">My Grocery List</h1>
 
       <ul id="PantryList">
         {Object.keys(userGroceryList).map((key, index) => (
@@ -77,18 +166,16 @@ function GroceryList() {
             <span id="PantryListItem" className="p-1 pl-5">{key}</span>
             <a onClick={() => deleteGroceryItem(key)}>
               <div className="bg-red-700 flex align-center justify-center rounded-full">  
-                <span id="PantryListDelete" className="pr-3 m-0 text-white">
-                  X
-                </span>
+                <span id="PantryListDelete" className="pr-3 m-0 text-white">X</span>
               </div>
             </a>
+            <button onClick={() => setSelectedItem(key)}>Check Footprint</button> 
           </li>
         ))}
       </ul>
 
       <label htmlFor="FoodName" className="font-semibold">
-        Food Name: 
-
+        Food Name:
         <input
           type="text"
           value={foodEntry}
@@ -97,14 +184,14 @@ function GroceryList() {
         />
       </label>
 
-      <button
-        onClick={handleSubmit}
-        className="w-full justify-center rounded-md bg-green-600 p-2 my-3 text-sm font-semibold leading-6 text-black shadow-lg hover:bg-amber-500"
-      >
+      <button onClick={handleSubmit} className="w-full justify-center rounded-md bg-green-600 p-2 my-3 text-sm font-semibold leading-6 text-black shadow-lg hover:bg-amber-500">
         Add Item To Shopping List
       </button>
+
+      {selectedItem && <FoodPrintApi query={selectedItem} />} 
     </div>
   );
 }
 
 export default GroceryList;
+
