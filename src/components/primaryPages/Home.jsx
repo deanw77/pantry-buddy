09b1d5966e4487818c4c04195599e8db63938cd0
deanw77 <deanwaring77@outlook.com>
2024-02-12 18:04:51 +0000

    Get recipe based on shortest expiry date from Pantry

--- src/components/primaryPages/Home.jsx
+++ src/components/primaryPages/Home.jsx
@@ -5,13 +5,12 @@ import "../css/widget.css";
 
 import PantryList from "../widgets/PantryList";
 import PieCharts from "../widgets/PieChart";
-import GroceryListWidget from "../widgets/GroceryListWidget"
-import SpoonacularApi from "../widgets/SpoonacularApi"
+import GroceryListWidget from "../widgets/GroceryListWidget";
+import SpoonacularApi from "../widgets/SpoonacularApi";
 import RecipeCard from "../widgets/RecipeCard";
-import SearchBarRecipe from "../widgets/SearchBarRecipe"
+import SearchBarRecipe from "../widgets/SearchBarRecipe";
 
 function Home() {
-
   const [query, setQuery] = useState("");
 
   const handleSearch = (newQuery) => {
@@ -20,32 +19,29 @@ function Home() {
 
   return (
     <div id="widgetContainer" className="bg-amber-50 flex">
+      <div className=" grid grid-cols-1 lg:grid-cols-3 gap-4 w-full">
+        <div className="col-start-1 col-span-1 lg:col-span-1 rounded shadow-lg justify-center bg-white">
+          <PieCharts />
+        </div>
 
-    <div className=" grid grid-cols-1 lg:grid-cols-3 gap-4 w-full">
+        <div className="col-span-1 lg:col-start-2 rounded shadow-lg justify-center bg-white">
+          <PantryList />
+        </div>
 
-      <div className="col-start-1 col-span-1 lg:col-span-1 rounded shadow-lg justify-center bg-white">
-        <PieCharts />
-      </div>
+        <div className="col-span-1 rounded shadow-lg justify-center bg-white">
+          <GroceryListWidget />
+        </div>
 
-      <div className="col-span-1 lg:col-start-2 rounded shadow-lg justify-center bg-white">
-        <PantryList/>
-      </div>
-
-      <div className="col-span-1 rounded shadow-lg justify-center bg-white">
-        <GroceryListWidget />
-      </div>
-      
-      <div className="col-span-1 lg:col-span-2 rounded shadow-lg justify-center bg-white">
+        <div className="col-span-1 lg:col-span-2 rounded shadow-lg justify-center bg-white">
           <SpoonacularApi query={query} />
           <RecipeCard />
         </div>
 
         <div className="col-span-1 rounded shadow-lg justify-center bg-white">
-          <SearchBarRecipe onSearch = {handleSearch} />
-          
+          <SearchBarRecipe onSearch={handleSearch} />
         </div>
+      </div>
     </div>
-  </div>
   );
 }
 
