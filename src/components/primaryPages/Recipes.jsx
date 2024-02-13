e084c6cfb5576c69a33a65c71442f106dada680b
Nami <cenami@live.fr>
2024-02-13 21:06:31 +0000

    adding recipes to recipe component

--- src/components/primaryPages/Recipes.jsx
+++ src/components/primaryPages/Recipes.jsx
@@ -1,12 +1,18 @@
-import RecipeCard from '../widgets/RecipeCard'
+import React from 'react';
+import SpoonacularApi from '../widgets/SpoonacularApi';
+// import axios from 'axios';
+// import RecipeCard from '../widgets/RecipeCard'
 
-function Recipes() {
-    return ( 
-        <div id="widgetContainer">
-            <h1>Recipes Display</h1>
-            <RecipeCard />
-        </div>
-     );
+const Recipes = () => {
+
+    return(
+<div className='col-span-1 lg:col-span-2 rounded shadow-lg justify-center bg-white'>
+<SpoonacularApi query={"beans"}/>
+</div>
+    )
 }
+
+
+    
 
 export default Recipes;
