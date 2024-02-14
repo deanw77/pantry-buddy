ada8c2496f43646af0f30dc5413e64bf6d3fce58
Alexandra Asanache <ale.asanache@gmail.com>
2024-02-14 13:10:47 +0000

    Moved recipes components from GroceryList to Recipes pages

--- src/components/primaryPages/GroceryList.jsx
+++ src/components/primaryPages/GroceryList.jsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
-
 import "../css/widget.css";
 
 import GroceryListWidget from "../widgets/GroceryListWidget";
 import PantryList from "../widgets/PantryList";
-import SearchBarRecipe from "../widgets/SearchBarRecipe";
-import SpoonacularApi from "../widgets/SpoonacularApi";
-import RecipeCard from "../widgets/RecipeCard";
 
-function GroceryList() {
-  const [query, setQuery] = useState("");
-
-  const handleSearch = (newQuery) => {
-    setQuery(newQuery);
-  };
-
+export default function GroceryList() {
   return (
     <div id="widgetContainer" className="bg-amber-50 flex">
 
@@ -27,20 +16,7 @@ function GroceryList() {
         <div className="col-span-1 lg:col-start-3 rounded shadow-lg justify-center bg-white">
           <PantryList />
         </div>
-
-        <div className="col-span-1 rounded shadow-lg justify-center bg-white">
-          <SearchBarRecipe onSearch = {handleSearch} />
-          
-        </div>
-        
-        <div className="col-span-1 lg:col-span-2 rounded shadow-lg justify-center bg-white">
-          <SpoonacularApi query={query} />
-          <RecipeCard />
-        </div>
-
       </div>
     </div>
   );
 }
-
-export default GroceryList;
