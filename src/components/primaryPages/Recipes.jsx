827b21375c22648d1010609f91fae2a0af5b1737
Nami <cenami@live.fr>
2024-02-14 07:49:03 +0000

    removing commented unsued code and ApiCall component, data component

--- src/components/primaryPages/Recipes.jsx
+++ src/components/primaryPages/Recipes.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import SpoonacularApi from '../widgets/SpoonacularApi';
-// import axios from 'axios';
-// import RecipeCard from '../widgets/RecipeCard'
+
 
 const Recipes = () => {
 
-    return(
-<div className='col-span-1 lg:col-span-2 rounded shadow-lg justify-center bg-white'>
-<SpoonacularApi query={"beans"}/>
-</div>
+    return (
+        <div className='col-span-1 lg:col-span-2 rounded shadow-lg justify-center bg-white'>
+            <SpoonacularApi query={"beans"} />
+        </div>
     )
 }
-
-
-    
 
 export default Recipes;
